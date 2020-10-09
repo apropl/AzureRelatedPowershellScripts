35808a1e39ae814782cfdc673ecceb17da383fa4
Richard Tomstad <Richard.Tomstad@sj.se>
2020-10-09 11:06:31 +0200

    Added .TrimStart('/') in case the pathDeptsToApi = 0

--- APIMgmt/PassthroughAPIM/passthroughAPIM.ps1
+++ APIMgmt/PassthroughAPIM/passthroughAPIM.ps1
@@ -316,7 +316,7 @@ if($setYamlPathToClipboard)
     Write-Host "Setting Azure Devops friendly path of yaml pipeline to clipboard" -ForegroundColor Green # -BackgroundColor white
 
     $relativePath = ( $outputDirectory -split '\\' | select -last $pathDepthToApi ) -join '/'
-    $yamlpath = "$relativePath/api-$apiName/api-$apiName.pipeline.yml"
+    $yamlpath = "$relativePath/api-$apiName/api-$apiName.pipeline.yml".TrimStart('/')
     Set-Clipboard -Value $yamlpath
 
 }
